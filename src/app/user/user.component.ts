2d18eb32b4301696cfbf409102d7af597adbe655
wasif <wibrar@mun.ca>
2025-03-06 16:58:40 -0330

    newer method of outputs

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, signal, Input, Output, EventEmitter } from '@angular/core';
+import { Component, computed, signal, Input, Output, EventEmitter, output } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
 @Component({
@@ -12,7 +12,8 @@ export class UserComponent {
   @Input({required: true}) id!: string;
   @Input({required: true}) avatar!: string; 
   @Input({required: true}) name!: string;
-  @Output() select = new EventEmitter();
+  // @Output() select = new EventEmitter();
+  select = output<string>();
 
   
   get imagePath() {
