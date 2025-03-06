4a26969785322c9dc3d82d0a7887d4ad53559e8c
wasif <wibrar@mun.ca>
2025-03-06 15:53:16 -0330

    how signals works, and also changed the structure of the app, using @input decorators

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,7 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal, Input } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -10,19 +9,14 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex];
-
-
-  get imagePath()
-  {
-    return '/assets/users/' + this.selectedUser.avatar
-    
+  @Input() avatar!: string; 
+  @Input() name!: string;
+  
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
   }
-
   onSelectUser() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
-    this.selectedUser = DUMMY_USERS[randomIndex];
-    console.log("Clicked!");
-    console.log("what are signals?");
+    
+    
   }
 }
