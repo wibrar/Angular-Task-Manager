752812b3d382b89188dc495406b1036e7667581f
wasif <wibrar@mun.ca>
2025-03-06 15:58:16 -0330

    how required inputs works

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -9,8 +9,8 @@ import { DUMMY_USERS } from '../dummy-users';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  @Input() avatar!: string; 
-  @Input() name!: string;
+  @Input({required: true}) avatar!: string; 
+  @Input({required: true}) name!: string;
   
   get imagePath() {
     return 'assets/users/' + this.avatar;
