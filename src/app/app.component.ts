448d8a1d20295dd1e7d3ab06077b3bfd70aeefba
wasif <wibrar@mun.ca>
2025-03-07 18:53:40 -0330

    added another component for displaying tasks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,26 @@
 import { Component } from '@angular/core';
-
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from "./user/user.component";
+import { TasksComponent } from './tasks/tasks.component';
 import { DUMMY_USERS } from './dummy-users';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   users = DUMMY_USERS;
+  selectedUserId = "u1";
 
+  get selectedUser() {
+    return this.users.find((user) => this.user.id === this.selectedUserId)!;
+  }
+
+  
   onSelectUser(id: string){
-    console.log("User Clicked! User ID:" + id);
+    this.selectedUserId = id;    
   };
 }
