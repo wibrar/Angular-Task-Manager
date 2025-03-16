963b5c5e6a86f29456f5dfd6d8d1fdc1e920f0b6
wasif <wibrar@mun.ca>
2025-03-15 23:08:09 -0230

    service

---
+++ src/app/tasks/tasks.service.ts
@@ -0,0 +1,48 @@
+import { NewTaskdData, Task } from "./task/task.model";
+
+class TasksService {
+    private tasks = [
+        {
+          id: 't1',
+          userId: 'u1',
+          title: 'Master Angular',
+          summary:
+            'Learn all the basic and advanced features of Angular & how to apply them.',
+          dueDate: '2025-12-31',
+        },
+        {
+          id: 't2',
+          userId: 'u3',
+          title: 'Build first prototype',
+          summary: 'Build a first prototype of the online shop website',
+          dueDate: '2024-05-31',
+        },
+        {
+          id: 't3',
+          userId: 'u3',
+          title: 'Prepare issue template',
+          summary:
+            'Prepare and describe an issue template which will help with project management',
+          dueDate: '2024-06-15',
+        },
+    ]
+
+    getUsertasks(userId: string){
+        return this.tasks.filter((task) => userId === userId);
+    }
+
+    addTask(taskData: NewTaskdData, userId: string){
+        this.tasks.unshift({
+            id: new Date().getTime().toString(),
+            userId: userId,
+            title: taskData.title,
+            summary: taskData.summary,
+            dueDate: taskData.date,
+          });
+    }
+
+    removeTask(id: string){
+        this.tasks = this.tasks.filter((task) => task.id !== id);
+
+    }
+}
